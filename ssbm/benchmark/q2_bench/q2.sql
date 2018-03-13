56d05dbc8807b18d8f6631dbbdd6aae4580f51ee
Jan Hofmeier <jan.hofmeier@recondita.de>
2018-03-13 16:12:21 +0100

    queries

---
+++ ssbm/benchmark/q2_bench/q2.sql
@@ -0,0 +1,37 @@
+select sum(lo_revenue), d_year, p_brand
+from lineorder
+join dim_date
+ on lo_orderdatekey = d_datekey
+join part
+on lo_partkey = p_partkey join supplier
+on lo_suppkey = s_suppkey
+where  p_category = 'MFGR#12'
+and s_region = 'AMERICA'
+group by d_year, p_brand
+order by d_year, p_brand;
+
+select sum(lo_revenue), d_year, p_brand
+from lineorder
+join dim_date
+on lo_orderdatekey = d_datekey
+join part
+on lo_partkey = p_partkey
+join supplier
+on lo_suppkey = s_suppkey
+where  p_brand between 'MFGR#2221' and 'MFGR#2228'
+and s_region = 'ASIA'
+group by d_year, p_brand
+order by d_year, p_brand;
+
+select sum(lo_revenue), d_year, p_brand
+from lineorder
+join dim_date
+on lo_orderdatekey = d_datekey
+join part
+on lo_partkey = p_partkey
+join supplier
+on lo_suppkey = s_suppkey
+where  p_brand= 'MFGR#2239'
+and s_region = 'EUROPE'
+group by d_year, p_brand
+order by d_year, p_brand;
---
+++ ssbm/benchmark/q4_bench/q4.sql
@@ -0,0 +1,57 @@
+select d_year, c_nation,
+sum(lo_revenue - lo_supplycost) as profit
+from lineorder
+join dim_date
+  on lo_orderdatekey = d_datekey
+join customer
+  on lo_custkey = c_customerkey
+join supplier
+  on lo_suppkey = s_suppkey
+join part
+  on lo_partkey = p_partkey
+where
+c_region = 'AMERICA'
+and s_region = 'AMERICA'
+and (p_mfgr = 'MFGR#1'
+or p_mfgr = 'MFGR#2')
+group by d_year, c_nation
+order by d_year, c_nation;
+
+select d_year, s_nation, p_category,
+sum(lo_revenue - lo_supplycost) as profit
+from lineorder
+join dim_date
+  on lo_orderdatekey = d_datekey
+join customer
+  on lo_custkey = c_customerkey
+join supplier
+  on lo_suppkey = s_suppkey
+join part
+  on lo_partkey = p_partkey
+where
+c_region = 'AMERICA'
+and s_region = 'AMERICA'
+and (d_year = 1997 or d_year = 1998)
+and (p_mfgr = 'MFGR#1'
+or p_mfgr = 'MFGR#2')
+group by d_year, s_nation, p_category
+order by d_year, s_nation, p_category;
+
+select d_year, s_city, p_brand,
+sum(lo_revenue - lo_supplycost) as profit
+from lineorder
+join dim_date
+  on lo_orderdatekey = d_datekey
+join customer
+  on lo_custkey = c_customerkey
+join supplier
+  on lo_suppkey = s_suppkey
+join part
+  on lo_partkey = p_partkey
+where
+s_nation = 'UNITED STATES'
+and (d_year = 1997 or d_year = 1998)
+and p_category = 'MFGR#14'
+group by d_year, s_city, p_brand
+order by d_year, s_city, p_brand;
+
